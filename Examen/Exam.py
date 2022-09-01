4773e680ba37cfac4381d50cf56a256aceec144a
yuli <belotkach.test@gmail.com>
2022-09-01 20:48:37 +0300

    2 задачи сделаны

---
+++ Examen/Exam.py
@@ -0,0 +1,16 @@
+#1. Напишите функцию, которая будет принимать номер кредитной карты и
+#показывать только последние 4 цифры. Остальные цифры должны заменяться
+#звездочками
+def kartochka(card):
+    return '*' * len(card[:-4]) + card[-4:]
+
+print(kartochka('1626575781724356'))
+
+#2. Напишите функцию, которая проверяет: является ли слово палиндромом
+def palindrome(str):
+    str = str.replace(' ', '').lower()
+    return 'Палиндром' if str == str[::-1] else 'Не палиндром'
+
+print(palindrome('lllfhhhdhhf'))
+print(palindrome('vnnvnvnnvnv'))
+print(palindrome('12233221'))
